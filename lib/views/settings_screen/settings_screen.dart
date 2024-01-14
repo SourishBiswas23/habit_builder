890334f76b1e925234221246629c18e103f59a8c
SourishBiswas23 <sourish135@gmail.com>
2024-01-15 03:19:12 +0530

    Added the settings screen

--- lib/views/settings_screen/settings_screen.dart
+++ lib/views/settings_screen/settings_screen.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:habit_builder/controllers/auth_bloc/auth_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../controllers/auth_bloc/auth_bloc.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: GestureDetector(
-          onTap: () {
-            context.read<AuthBloc>().add(SignOutEvent());
-          },
-          child: const Text('This is the settings screen'),
-        ),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: () {
+              context.read<AuthBloc>().add(SignOutEvent());
+            },
+            child: Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Sign Out'),
+                  FaIcon(FontAwesomeIcons.chevronRight)
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
